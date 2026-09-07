61ead873c7cbe4bedc047f667d73a12f72d842b9
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-09-07 15:03:25 +0100

    feat: create User class with construct and getters

---
+++ src/main/java/ma/youcode/lineperm/model/User.java
@@ -0,0 +1,21 @@
+package ma.youcode.lineperm.model;
+
+public class User {
+    
+    private final String login;
+    private final String passwordHash;
+
+    public User(String login, String passwordHash){
+
+        this.login = login;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public String getPasswoordHash(){
+        return passwordHash;
+    }
+}
