0dcf7fbeb92bdbdb537d14e37c8988c7d6e51643
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-09-07 17:07:47 +0100

    feat: connect signup flow to user service

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -11,30 +11,9 @@ public class Main{
 
     public static void main(String[] args){
 
-        // ConsoleApp app = new ConsoleApp();
-        // app.run();
+        ConsoleApp app = new ConsoleApp();
+        app.run();
 
-       User user = new User("yassine", "hash123");
-
-       UserService userService = new UserService();
-
-       userService.addUser(user);
-
-       System.out.println(userService.userExists("yassine"));
-
-       User userFound = userService.getUser("yassine");
-
-       System.out.println(userFound.getLogin());
-       System.out.println(userFound.getPasswoordHash());
-
-       userService.createUser("ahmed@gmail.com", "1234321");
-
-       boolean ifouf = userService.userExists("ahmed@gmail.com");
-
-       System.out.println(ifouf);
-
-        boolean islogin =  userService.login("ahmed@gmail.com", "1234321");
-
-       System.out.println(islogin);
+       
     }
 }
--- src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
+++ src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
@@ -2,21 +2,41 @@ package ma.youcode.lineperm.ui;
 
 import java.util.Scanner;
 
+import ma.youcode.lineperm.service.UserService;
+
 public class ConsoleApp{
 
     private Scanner scanner = new Scanner(System.in);
+    private final UserService userService = new UserService();
+
+
 
     public void run(){
 
         while (true) {
-            System.out.println("linperm> ");
+            System.out.print("linperm> ");
             
             String command = scanner.nextLine();
             
             switch (command) {
 
                 case "signup":
-                    System.out.println("signup");
+                    
+                    System.out.println("Login: ");
+                    String login = scanner.nextLine();
+
+                    System.out.println("mot de passe: ");
+                    String password = scanner.nextLine();
+
+                    boolean created = userService.createUser(login, password);
+
+                    if(created){
+                        System.out.println("compte cree");
+                    }
+                    else{
+                        System.out.println("Login deja pris");
+                    }
+
                     break;
 
                 case "login":
