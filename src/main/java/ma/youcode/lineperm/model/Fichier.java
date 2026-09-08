3cb4670036fd49407e08aacb6bd62ec49dd3d909
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-09-08 16:41:53 +0100

    feat: add Fichier model with owner and permissions

---
+++ src/main/java/ma/youcode/lineperm/model/Fichier.java
@@ -0,0 +1,50 @@
+package ma.youcode.lineperm.model;
+
+public class Fichier {
+
+    private final String name;
+    private String content;
+    private final String owner;
+    private final String ownerPermissions;
+    private String othersPermissions;
+
+    public Fichier(String name, String owner){
+
+        this.name = name;
+        this.owner = owner;
+
+        this.ownerPermissions = "rwd";
+        this.othersPermissions = "---";
+        this.content = "";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public String getOwner(){
+        return owner;
+    }
+
+    public String getOwnerPermissions(){
+        return ownerPermissions;
+    }
+
+    public String getOthersPermissions(){
+        return othersPermissions;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public void setOthersPermissions(String othersPermissions){
+        this.othersPermissions = othersPermissions;
+    }
+
+    
+}
