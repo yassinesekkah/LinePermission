caa9eb110e9218e5e6e2106c36f6f567df12a3bb
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-09-21 12:15:10 +0000

    US-01: connect application to SQLite

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -1,26 +1,27 @@
 package ma.youcode.lineperm;
 
+import java.sql.Connection;
+
+import ma.youcode.lineperm.database.DBConnection;
 import ma.youcode.lineperm.ui.ConsoleApp;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ConsoleApp app = new ConsoleApp();
-        app.run();
+        // ConsoleApp app = new ConsoleApp();
+        // app.run();
 
-        // UserService userService = new UserService();
+        Connection connection = DBConnection.getInstance().getConnection();
 
-        // userService.loadUsers();
+        if(connection != null){
+            System.out.println("Connexion SQLite réussie");
+        }
+        else{
+            System.out.println("Connexion échouée");
+        }
 
-        // LogService logService = new LogService();
 
-        // Optional<AccessLog> result = logService.parseLine("2026-09-14;yassine;LECTURE;gg.txt;OK");
-
-        // System.out.println(result.isPresent());
-
-        // logService.loadLogs();
-        // System.out.println("Nombre de logs : " + logService.getLogs().size());
         
        
     }
--- src/main/java/ma/youcode/lineperm/database/DBConnection.java
+++ src/main/java/ma/youcode/lineperm/database/DBConnection.java
@@ -1,16 +1,35 @@
 package ma.youcode.lineperm.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
     private static DBConnection instance;
+    private Connection connection;
 
-    private DBConnection(){}
+    private static final String URL = "jdbc:sqlite:auditdb.db";
 
-    public static DBConnection getInstance(){
+    private DBConnection() {
 
-        if(instance == null){
+        try{
+            connection = DriverManager.getConnection(URL);
+        }
+        catch(SQLException e){
+            System.out.println("Erreur de connexion à la base de données");
+        }
+    }
+
+    public static DBConnection getInstance() {
+
+        if (instance == null) {
             instance = new DBConnection();
         }
         return instance;
+    }
+
+    public Connection getConnection(){
+        return connection;
     }
 }
