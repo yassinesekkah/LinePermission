9dd9c663a237f031f53e6d1df48db624ab4bc08a
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-09-24 15:17:58 +0000

    US-07: implement log lookup by id

--- src/main/java/ma/youcode/lineperm/dao/LogDao.java
+++ src/main/java/ma/youcode/lineperm/dao/LogDao.java
@@ -1,13 +1,23 @@
 package ma.youcode.lineperm.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Optional;
 
+import ma.youcode.lineperm.model.ActionTypes;
+import ma.youcode.lineperm.model.Fichier;
 import ma.youcode.lineperm.model.Log;
+import ma.youcode.lineperm.model.Status;
+import ma.youcode.lineperm.model.User;
 
 public class LogDao extends AbstractDao<Log>{
+
+    UserDao userDao = new UserDao();
+    FichierDao fichierDao = new FichierDao();
 
     @Override 
     public void save(Log log){
@@ -41,7 +51,67 @@ public class LogDao extends AbstractDao<Log>{
         }
     }
 
+    @Override 
     public Optional<Log> findById(int id){
+
+        String sql = """
+                    SELECT * FROM logs
+                    WHERE id = ?;
+                """;
+
+        try(PreparedStatement statement = con.prepareStatement(sql)){
+            
+            statement.setInt(1, id);
+            ResultSet result = statement.executeQuery();
+
+            if(result.next()){
+                int userId = result.getInt("user_id");
+                //user prepare
+                Optional<User> userOptional = userDao.findById(userId);
+
+                if(userOptional.isEmpty()){
+                    System.out.println("Erreur lors trouver utilisateur");
+                    return Optional.empty();
+                }
+                
+                User user = userOptional.get();
+
+                //fichier object prepare
+                int fichierId = result.getInt("fichier_id");
+
+                Optional<Fichier> fichierOptional = fichierDao.findById(fichierId);
+
+                if(fichierOptional.isEmpty()){
+                    System.out.println("Erreur lors trouver le fichier");
+                    return Optional.empty();
+                }
+
+                Fichier fichier = fichierOptional.get();
+
+
+
+                String actionString = result.getString("action");
+                ActionTypes action = ActionTypes.valueOf(actionString);
+
+                String resultatString = result.getString("resultat");
+                Status resultat = Status.valueOf(resultatString);
+
+                //prepare date & time
+                String createdAtString = result.getString("created_at");
+                LocalDateTime createdAt = LocalDateTime.parse(createdAtString);
+                LocalDate date = createdAt.toLocalDate();
+                LocalTime time = createdAt.toLocalTime();
+
+                //creation d'objet log
+                Log log = new Log(id, date, time,user , action, fichier, resultat);
+
+                return Optional.of(log);
+
+            }
+
+        }catch(SQLException e){
+            System.out.println("Erreur lors trouver log");
+        }
 
         return Optional.empty();
     }
